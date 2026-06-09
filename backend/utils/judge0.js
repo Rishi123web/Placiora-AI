afbb05e13e9392ec89b49bf4e8a9a829b3f9dcf3
Rishi Jaiswal <arquad123@gmail.com>
2026-06-09 12:15:16 +0530

    Debug Judge0 error

--- backend/utils/judge0.js
+++ backend/utils/judge0.js
@@ -18,21 +18,33 @@ export async function runJudge0({ code, language, stdin = "" }) {
     throw new Error("Unsupported language")
   }
 
-  const response = await axios.post(
-    `${JUDGE0_URL}/submissions?base64_encoded=false&wait=true`,
-    {
-      source_code: code,
-      language_id: languageId,
-      stdin
-    },
-    {
-      timeout: 30000,
-      headers: {
-        "Content-Type": "application/json",
-        "ngrok-skip-browser-warning": "true"
+  try {
+    const response = await axios.post(
+      `${JUDGE0_URL}/submissions?base64_encoded=false&wait=true`,
+      {
+        source_code: code,
+        language_id: languageId,
+        stdin
+      },
+      {
+        timeout: 30000,
+        headers: {
+          "Content-Type": "application/json",
+          "ngrok-skip-browser-warning": "true"
+        }
       }
-    }
-  )
+    )
 
-  return response.data
+    return response.data
+  } catch (error) {
+    console.log("Judge0 Full Error:", error.response?.data || error.message)
+
+    throw new Error(
+      JSON.stringify({
+        status: error.response?.status,
+        data: error.response?.data,
+        message: error.message
+      })
+    )
+  }
 }
